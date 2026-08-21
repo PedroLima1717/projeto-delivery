2a8707f2d0bff8b22fddb02b8fb4060b05f5e397
Pedro Henrique Lima dos Santos <pedro.h.santos241@aluno.senai.br>
2026-08-21 13:55:59 -0300

    Atividade 2

---
+++ atividade2-desconto/src/App.jsx
@@ -0,0 +1,8 @@
+import TagDesconto from "./components/TagDesconto";
+
+function App() {
+    return (
+        <><TagDesconto porcentagem={20} /><TagDesconto porcentagem={50} /></>
+    )
+}
+export default App
---
+++ atividade2-desconto/src/components/TagDesconto.jsx
@@ -0,0 +1,11 @@
+function TagDesconto({ porcentagem }) {
+
+    return (
+        <div style={{
+            backgroundColor: 'red',
+            color: 'white',}}>
+            <span>🔥 {porcentagem}% OFF</span>
+        </div>
+    );
+}
+export default TagDesconto
